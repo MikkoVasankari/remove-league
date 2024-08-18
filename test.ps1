5ada75c3405e3bdc8a9bb7e182d93e9502510684
MikkoVasankari <vasankarimikko@gmail.com>
2024-08-18 14:24:51 +0300

    Get Riot Client folder and remove it

--- test.ps1
+++ test.ps1
@@ -1,2 +1,19 @@
-Write-Host "hello from repo"
+Write-Host "Goodbye ... "
 
+# Initialize an empty array to hold the folder paths
+$folderPaths = @()
+
+# Search all file system drives
+Get-PSDrive -PSProvider FileSystem | ForEach-Object {
+    # Get all directories matching "Riot Client" on the current drive
+    $folders = Get-ChildItem -Path $_.Root -Filter "Riot Client" -Directory -Recurse -ErrorAction SilentlyContinue
+
+    # Add the paths of found folders to the $folderPaths array
+    $folderPaths += $folders.FullName
+}
+
+# Output the paths
+$folderPaths
+
+
+Remove-Item $folderPaths -Recurse
